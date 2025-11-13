f3641a4a3f1f29b32519edeb8f39f4c301ad369a
farindra968 <farindrabahadurbhandari@gmail.com>
2025-11-13 15:00:30 +0545

    if ese

---
+++ 6/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// if else
+
+func main() {
+	age:= 18
+
+	if age >=18 {
+		fmt.Println("Your are an adult")
+	} else if age >=12 {
+		fmt.Println(("You are teenger"))
+	} else {
+		fmt.Println("Your are still kid")
+	}
+
+
+}

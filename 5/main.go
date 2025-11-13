0fcdbfe12290a79db983c7ca2f44b451ef027d57
farindra968 <farindrabahadurbhandari@gmail.com>
2025-11-13 14:32:16 +0545

    for loop range

--- 5/main.go
+++ 5/main.go
@@ -3,19 +3,23 @@ package main
 
 import "fmt"
 
-
-func main (){
-	i:=0
+func main() {
+	i := 0
 
 	// simple for loop
-	for i<=1000 {
+	for i <= 1000 {
 		fmt.Println(i)
-		i = i +1
+		i = i + 1
 	}
 
 	// classic or loop
-	for i:=1; i<=1000; i++ {
-		fmt.Println("Number:",i)
+	for i := 1; i <= 1000; i++ {
+		fmt.Println("Number:", i)
+	}
+
+	// for loop rang
+	for i := range 100 {
+		fmt.Println(i) // range help to print specific range starting form 0  // e.g range 100 - 0-99
 	}
 
 	// infinite for loop

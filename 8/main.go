03cba0b12adf0a776e478841d632e135612f80d2
farindra968 <farindrabahadurbhandari@gmail.com>
2025-11-15 07:02:17 +0545

    array

--- 8/main.go
+++ 8/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main(){
-	
+import "fmt"
+
+func main() {
+	var nums [4]int
+	nums[0]=1
+
+	// prnt full array
+	fmt.Println(nums)
+	// array indx prin
+	fmt.Println(nums[0])
+	// Array lengh print
+	fmt.Println(len(nums))
+
+	// int
+	age := [3]int{20, 24,26}
+	name := [3]string{"Ram","Sita","Gita"}
+
+	fmt.Println(name, age)
+
+	// 2d array
+	value := [2][2]int{{1,3},{2,4}}
+	fmt.Println(value)
+
 }

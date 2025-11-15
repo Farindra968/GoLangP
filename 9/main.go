f41e16051d58bc28d6a20402f1587e0d0322d7a3
farindra968 <farindrabahadurbhandari@gmail.com>
2025-11-15 07:34:11 +0545

    slice

---
+++ 9/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// uninitialized slice is deaul nill or null
+	// value := []int
+	name := []string{"Ram", "Shyam", "Hari", "Laxuman"}
+	fmt.Println(cap(name)) // maxium capacity of he slice / like .length in JS
+	fmt.Println(name)
+}

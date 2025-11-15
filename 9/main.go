f184324d26e357e7fb5c8f9ce7dc2e54b9f9b335
farindra968 <farindrabahadurbhandari@gmail.com>
2025-11-15 07:34:59 +0545

    .

--- 9/main.go
+++ 9/main.go
@@ -6,6 +6,6 @@ func main() {
 	// uninitialized slice is deaul nill or null
 	// value := []int
 	name := []string{"Ram", "Shyam", "Hari", "Laxuman"}
-	fmt.Println(cap(name)) // maxium capacity of he slice / like .length in JS
+	fmt.Println(cap(name)) // maxium capacity of the slice / like .length in JS
 	fmt.Println(name)
 }
